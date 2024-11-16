28b98a75a595d2a00b5329053abd2c1d1954f727
Ismaeil <ismaeilsaberi@gmail.com>
2024-11-15 19:11:39 -0800

    creating structure of pages and components and adding dark theme with the help of styled componenets

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,34 @@
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Theme";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: 3;
+`;
+
 function App() {
-  return <div className="App">Heloo</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container>
+        <Wrapper>Hello</Wrapper>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
