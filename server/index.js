2f4a5846a51bd6765e937f64627e030eed327074
Ismaeil <ismaeilsaberi@gmail.com>
2024-11-17 10:06:01 -0800

    making the main structure of express server and installing necessary packages

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config;
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(status).json({ success: false, status, message });
+});
+
+// Default get
+app.get("/", async (req, res) => {
+  res.status(200).json({ message: "Hello AI saas developers!" });
+});
+
+// function to start the server
+const startServer = async () => {
+  try {
+    app.listen(8080, () => console.log("Server is running on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
